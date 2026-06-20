20793f9d19715c07ba8d6fd6b13d10e9abd6f227
arun mohapatra <mohapatraarun64@gmail.com>
2026-06-20 22:29:27 +0530

    Correct

--- expense/src/main/java/com/expense/expense/config/CorsConfig.java
+++ expense/src/main/java/com/expense/expense/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 
         config.setAllowedOrigins(
                 List.of("http://localhost:5173"
-                        ,"https://expense-tracker-frontend-five-gamma.vercel.app/"
+                        ,"https://expense-tracker-frontend-five-gamma.vercel.app"
                 )
 
         );
